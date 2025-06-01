d413ed82d7df2220b5a4968e49bf4095e3038449
Hanan Abdimalik <hanan.abdimalik@autotrader.co.uk>
2025-06-01 18:11:25 +0100

    Add endpoint to update gameBoard

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -7,10 +7,16 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    val repo = NoughtAndCrossesRepository()
     routing {
         get("/gameBoard") {
-            val repo = NoughtAndCrossesRepository()
             call.respond(HttpStatusCode.OK, repo.gameBoard)
+        }
+
+        get("updateBoard/{position}") {
+            val position = call.parameters["position"]?.toInt() ?: 0
+            val updatedBoard = repo.updateGameBoard(position)
+            call.respond(HttpStatusCode.OK, updatedBoard)
         }
     }
 }
--- src/main/kotlin/model/NoughtAndCrossesRepository.kt
+++ src/main/kotlin/model/NoughtAndCrossesRepository.kt
@@ -2,5 +2,12 @@ package com.example.model
 
 class NoughtAndCrossesRepository {
 
-    var gameBoard = MutableList(9) { GameCell(GamePieces.Unplayed, it) }
+    val gameBoard = MutableList(9) { GameCell(GamePieces.Unplayed, it) }
+
+    fun updateGameBoard(position: Int): List<GameCell> {
+        if (position in 0 until gameBoard.size) {
+            gameBoard[position] = gameBoard[position].copy(GamePieces.Nought, position)
+        }
+        return gameBoard
+    }
 }
